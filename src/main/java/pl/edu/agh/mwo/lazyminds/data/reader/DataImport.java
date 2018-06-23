bf08e670ed333166dba9785dcebde2f815d60888
Paweł Kłósko <kloskopawel@gmail.com>
2018-06-23 20:18:16 +0200

    Reading data from Excel - work in progress

--- src/main/java/pl/edu/agh/mwo/lazyminds/data/reader/DataImport.java
+++ src/main/java/pl/edu/agh/mwo/lazyminds/data/reader/DataImport.java
@@ -2,20 +2,17 @@ package pl.edu.agh.mwo.lazyminds.data.reader;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.CellReference;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.ss.usermodel.Row;
+
 import org.apache.poi.ss.usermodel.Sheet;
 
 import pl.edu.agh.mwo.lazyminds.model.User;
@@ -23,102 +20,81 @@ import pl.edu.agh.mwo.lazyminds.model.WorkUnit;
 
 public class DataImport {
 
-	
-	public static void main(String[] args) throws IOException{
-		
+	public static void main(String[] args) throws IOException, ParseException {
+
 		ArrayList<String> listOfFiles = new ArrayList<String>();
-		listOfFiles.add("Kowalski_Jan.xls");
-		//listOfFiles.add("Nowak_Piotr.xls");
-		
+		ArrayList<User> listOfUsers = new ArrayList<User>();
+
+		listOfFiles.add("C:\\Users\\Pawe³\\git\\reports\\resources\\reporter-dane\\2012\\01\\Kowalski_Jan.xls"); 
+		listOfFiles.add("C:\\Users\\Pawe³\\git\\reports\\resources\\reporter-dane\\2012\\01\\Nowak_Piotr.xls");
+		listOfFiles.add("C:\\Users\\Pawe³\\git\\reports\\resources\\reporter-dane\\2012\\02\\Kowalski_Jan.xls");
+
 		for (String fileName : listOfFiles) {
+			List<String> rawWorkUnits = new ArrayList<String>();
+			WorkUnit workUnit = new WorkUnit();
 			FileInputStream file = new FileInputStream(new File(fileName));
-			
-			
 			HSSFWorkbook wb = new HSSFWorkbook(file);
 
-			
-			DataFormatter formatter = new DataFormatter();
-		    Sheet sheet1 = wb.getSheetAt(0);
-		    for (Row row : sheet1) {
-		        for (Cell cell : row) {
-		            CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
-		           
-		            String text = formatter.formatCellValue(cell);
-		        
-		            switch (cell.getCellType()) {
-		                
-		            case Cell.CELL_TYPE_STRING:
-		            	System.out.print(cell.getStringCellValue()+"\t");
-		            	break;
-		                case Cell.CELL_TYPE_NUMERIC:
-		                    if (DateUtil.isCellDateFormatted(cell)) {
-		                        System.out.print(cell.getDateCellValue()+"\t");
-		                    } else {
-		                        System.out.print(cell.getNumericCellValue()+"\t");
-		                    }
-		                    break;
-		                case Cell.CELL_TYPE_BOOLEAN:
-		                    System.out.print(cell.getBooleanCellValue()+"\t");
-		                    break;
-		               
-		            }
-		            
-		        }
-		    }System.out.println("\n");
-			
-		}	
-		
-		
-		
-		
-	}
-	
-	public User mapPersonFromExcel(String fineName) throws IOException{
-		
-		FileInputStream file = new FileInputStream(new File(fineName));
-		
-		HSSFWorkbook wb = new HSSFWorkbook(file);
-		User user = new User();
-		List<WorkUnit> workUnits = user.getWorkUnits();
-		workUnits= new ArrayList<WorkUnit>();
-		
-		DataFormatter formatter = new DataFormatter();
-	    Sheet sheet1 = wb.getSheetAt(0);
-	    for (Row row : sheet1) {
-	        for (Cell cell : row) {
-	            CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
-	           
-	            String text = formatter.formatCellValue(cell);
-	        
-	            switch (cell.getCellType()) {
-	                
-	            case Cell.CELL_TYPE_STRING:
-	            	System.out.print(cell.getStringCellValue()+"\t");
-	            	break;
-	                case Cell.CELL_TYPE_NUMERIC:
-	                    if (DateUtil.isCellDateFormatted(cell)) {
-	                        System.out.print(cell.getDateCellValue()+"\t");
-	                    } else {
-	                        System.out.print(cell.getNumericCellValue()+"\t");
-	                    }
-	                    break;
-	                case Cell.CELL_TYPE_BOOLEAN:
-	                    System.out.print(cell.getBooleanCellValue()+"\t");
-	                    break;
-	               
-	            }
-	            
-	        }
-	    }System.out.println("\n");
-		return user;
-		
-		
+			User currentUser = new User();
+			int index = fileName.lastIndexOf("\\");
+			String[] names = fileName.substring(index + 1).split("_");
+
+			currentUser.setName(names[0]);
+			currentUser.setSurname(names[1].replace(".xls", ""));
+			// System.out.println(currentUser.getName() + " " + currentUser.getSurname());
+			String name = currentUser.getName();
+			String surname = currentUser.getSurname();
+			if (listOfUsers.contains(currentUser)) {
+				currentUser = listOfUsers.get(listOfUsers.indexOf(currentUser));
+			} else {
+				listOfUsers.add(currentUser);
+			}
+
+			// System.out.println(listOfUsers.size());
+
+			int i = 0;
+
+			while (i < wb.getNumberOfSheets()) {
+				Sheet sheet = wb.getSheetAt(i);
+				int j = 1;
+
+				System.out.println("User: " + name + " " + surname + " - arkusz: " + i +"\n");
+
+				int numberOfRows = sheet.getPhysicalNumberOfRows();
+				int numberOfColumns = sheet.getRow(0).getPhysicalNumberOfCells();
+				// System.out.print(numberOfRows + " " + numberOfColumns);
+				while (j < numberOfRows) {
+
+					int k = 0;
+					String rawRow = "";
+					List<String> dates = new ArrayList<String>();
+					List<String> notes = new ArrayList<String>();
+					List<Double> hours = new ArrayList<Double>();
+
+					while (k <= numberOfColumns) {
+						try {
+							rawRow += (sheet.getRow(j).getCell(k).toString() + "|");
+						} catch (NullPointerException n) {
+							// System.out.println("Koniec zakresu");
+						}
+						k++;
+					}
+					rawRow += sheet.getSheetName();
+					// System.out.println(rawRow);
+					j++;
+					String[] splitted = rawRow.split("|");
+					// System.out.println(splitted.length);
+					// if (splitted.length == 4) {
+					// dates.add(splitted[0]);
+					// notes.add(splitted[1]);
+					// hours.add(Double.valueOf(splitted[2]));
+					// }
+
+					System.out.println(rawRow);
+					
+				}
+				i++;}
+
+		}
 	}
 }
-
-
-	
-	
-	
-	
-
