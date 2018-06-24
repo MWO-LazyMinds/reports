156a98397d00e356cb8dc2447074bbc074799934
student12 <student12@S7N-PROGLANG-20.pcoip.ki.agh.edu.pl>
2018-06-24 14:18:38 +0200

    Work in progress

--- src/main/java/pl/edu/agh/mwo/lazyminds/reports/ReportHoursOnProjectByEmployee.java
+++ src/main/java/pl/edu/agh/mwo/lazyminds/reports/ReportHoursOnProjectByEmployee.java
@@ -1,25 +1,52 @@
 package pl.edu.agh.mwo.lazyminds.reports;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import pl.edu.agh.mwo.lazyminds.model.Project;
 import pl.edu.agh.mwo.lazyminds.model.User;
 import pl.edu.agh.mwo.lazyminds.model.WorkUnit;
 
 public class ReportHoursOnProjectByEmployee {
 
-	public void generate(HashSet<User> users, int year) {
-
-		HashMap<User, WorkUnit> hmap = new HashMap<User, WorkUnit>();
-
+	@SuppressWarnings("deprecation")
+	public void generate(HashSet<User> users, int year, String employee) {
+		
+		HashMap<Project, Integer> projectAndHours = new HashMap<Project,Integer>();
+		HashMap<Project, Date> projectAndDates = new HashMap<Project,Date>();
+		
+		String[] splitted = employee.split("_");
+		String surname = splitted[0];
+		String name  =splitted[1];
+		//System.out.println(name+" "+surname);
 		for (User user : users) {
-
-			for (WorkUnit wk : user.getWorkUnits()) {
-
+			
+			if (user.getName().equals(name)&&user.getSurname().equals(surname)){
+				//System.out.println(user.getName()+" "+user.getSurname());
+				
+				for (WorkUnit wk : user.getWorkUnits()) {
+					//System.out.println(wk.getDate());
+					//System.out.println(wk.getDate().getYear());
+					if (wk.getDate().getYear()==year-1900){
+						System.out.println(wk.getProject().getName());
+						projectAndDates.put(wk.getProject(),wk.getDate());
+						projectAndHours.put(wk.getProject(),wk.getHours());
+				
+			}
+	
+			
+				
+				
+				
+			
+			}
+			
 			}
 
 		}
+		//System.out.println(projectAndDates.size());
 	}
 }
